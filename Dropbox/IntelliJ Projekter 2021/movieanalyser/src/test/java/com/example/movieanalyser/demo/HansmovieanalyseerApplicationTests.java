59510a6c0e32f664efa95b2cb2c72a2a8ac27505
Hans VMW <hans079d@stud.kea.dk>
2021-02-14 00:54:07 +0100

    first hns 1402 2021

---
+++ Dropbox/IntelliJ Projekter 2021/movieanalyser/src/test/java/com/example/movieanalyser/demo/HansmovieanalyseerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieanalyser.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansmovieanalyseerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
