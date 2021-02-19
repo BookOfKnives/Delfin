bc5a6ac2006e47273a0ffb14d0bd03e5b5fe7189
Hans VMW <hans079d@stud.kea.dk>
2021-02-19 16:50:47 +0100

    Hans' Movie Analyser Totalt hardcore https://www.youtube.com/watch?v=jdx6db7PPVs lidt musik til en tørstig mand på en fredag 1902 2021

---
+++ Dropbox/IntelliJ Projekter 2021/HansMovieDB02/src/main/java/com/movieanaly/HansMovie02Application.java
@@ -0,0 +1,17 @@
+package com.movieanaly;
+
+import com.movieanaly.controller.movieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+//hans forsøg 2 på movieapp 1602 2021
+@SpringBootApplication
+public class HansMovie02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HansMovie02Application.class, args);
+    }
+
+
+}
